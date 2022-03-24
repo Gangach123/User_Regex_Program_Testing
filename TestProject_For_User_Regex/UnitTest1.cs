2a2e883aebe9b93fe8510f188eba3c8d571c8d9a
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-24 22:11:52 +0530

    Test Cases for User Regex

---
+++ TestProject_For_User_Regex/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject_For_User_Regex
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ User_Regestartion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Regex_Problems
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to User Registration Regex Problems.");
+        }
+    }
+}
+
