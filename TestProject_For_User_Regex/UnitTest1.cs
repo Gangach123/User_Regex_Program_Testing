56c05f0e00f8ed9b398901b5cb6ccc4bcccc6d9c
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-25 13:00:53 +0530

    Exceptions for User_Regestration

--- TestProject_For_User_Regex/UnitTest1.cs
+++ TestProject_For_User_Regex/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Regex_Problems;
+using User_Regestartion;
 
 namespace TestProject_For_User_Regex
 {
@@ -8,60 +9,146 @@ namespace TestProject_For_User_Regex
     {
         Validation validation = new Validation();
         [TestMethod]
-        public void IfGiven_Name_Is_True_ReturnTrue()
+        public void GivenRight_FirstName_ReturnTrue()
         {
-            bool actual = validation.ValidateFirstName("Ganga");
-            bool expected = true;
+            string actual = validation.ValidateFirstName("Ganga");
+            string expected = "Valid firstName";
+
             Assert.AreEqual(expected, actual);
         }
+
         [TestMethod]
-        public void IfGiven_Name_Is_False_ReturnFalse()
+        public void GivenWrong_FirstName_ReturnFalse()
         {
-            bool actual = validation.ValidateFirstName("Ga");
-            bool expected = false;
+            try
+            {
+                string actual = validation.ValidateFirstName("ganga");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Invalid firstName", ex.Message);
+            }
+        }
+
+
+        [TestMethod]
+        public void GivenRight_LastName_ReturnTrue()
+        {
+            string actual = validation.ValidateLastName("Shiva");
+            string expected = "Valid lastName";
+
             Assert.AreEqual(expected, actual);
         }
+
         [TestMethod]
-        public void IfGiven_lastName_Is_True_ReturnTrue()
+        public void GivenWrong_LastName_ReturnFalse()
         {
-            bool actual = validation.ValidateLastName("Chilamakooru");
-            bool expected = true;
+            try
+            {
+                string actual = validation.ValidateLastName("Ch");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Invalid lastName", ex.Message);
+            }
+
+        }
+
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void GivenRight_Email_ReturnTrue(string EmailId)
+        {
+            try
+            {
+                string actual = validation.ValidateEmail(EmailId);
+  
+
+            }
+
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Valid email", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("abc@.com.my")]
+        [DataRow(".abc@abc.com")]
+        [DataRow("abc@abc@gmail.com")]
+        [DataRow("abc..2002@gmail.com")]
+        [DataRow("abc@gmail.com.1a")]
+        [DataRow("abc@gmail.com.aa.au")]
+        public void GivenWrong_Email_ReturnFalse(string EmailId1)
+        {
+            try
+            {
+                string actual = validation.ValidateEmail(EmailId1);
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Invalid email", ex.Message);
+            }
+        }
+
+
+        [TestMethod]
+        public void GivenRight_mobileNo_ReturnTrue()
+        {
+            string actual = validation.ValidateMobileNo("91 7702173235");
+            string expected = "Valid mobileNo";
+
             Assert.AreEqual(expected, actual);
         }
+
+
         [TestMethod]
-        public void IfGiven_lastName_Is_False_ReturnFalse()
+        public void GivenWrong_mobileNo_ReturnFalse()
         {
-            bool actual = validation.ValidateLastName("2h6");
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string actual = validation.ValidateMobileNo("7 702173235");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Invalid mobileNo", ex.Message);
+            }
         }
+
+
+
         [TestMethod]
-        public void IfGiven_mail_Is_True_ReturnTrue()
+        public void GivenRight_Password_ReturnTrue()
         {
-            bool actual = validation.Validating_Email("chilamakooru.gangaiiit@gmail.com");
-            bool expected = true;
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string actual = validation.ValidatePassword("abcDE#1234");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Valid password", ex.Message);
+            }
+            
         }
+
         [TestMethod]
-        public void IfGiven_mail_Is_False_ReturnFalse()
+        public void GivenWrong_Password_ReturnFalse()
         {
-            bool actual = validation.Validating_Email("chila@12@gmail.com");
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void IfGiven_number_Is_True_ReturnTrue()
-        {
-            bool actual = validation.ValidatingNumber("91 7702173235");
-            bool expected = true;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void IfGiven_number_Is_False_ReturnFalse()
-        {
-            bool actual = validation.ValidatingNumber("09 0977021732");
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string actual = validation.ValidatePassword("abCD12");
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual("Invalid password", ex.Message);
+            }
         }
     }
 }
--- User_Regestartion/Validation.cs
+++ User_Regestartion/Validation.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using User_Regestartion;
 
 namespace Regex_Problems
 {
@@ -12,78 +13,72 @@ namespace Regex_Problems
 
         public static string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
         public static string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
-        public static string emailId = "^[a-z]{3,}([.]{1}[a-z]{2,})?[/@/][a-z]{2,5}[/./][a-z]{2,5}([.]{1}[a-z]{2})?$";
+        public static string email = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$";
         public static string mobileNo = "^91[/ /][6-9]{1}[0-9]{9}$";
-        public static string password = "^[a-zA-Z0-9]{8,}";
+        public static string password = "^(?=.*[!@#$%^&*]{1})(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9@$!%*#?&]{8,}$";
         public static string password_2 = @"^(?=.*[A-Z])[[a-zA-Z1-9]{1}][[a-zA-Z0-9]{7,}]";
         public static string password_3 = @"^(?=.*[0-9])[a-zA-Z0-9]{8,}$";
         public static string password_4 = "^.*(?=.{8,})(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
         public static string mail = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$";
-        public void Validate(string data)
+        public string ValidateFirstName(string data)
         {
-            string result = Regex.IsMatch(data, mail) ? $"{data}: valid" : $"{data}: invalid";
-
-            Console.WriteLine(result);
+            bool result = Regex.IsMatch(data, firstName);
+            if (result)
+            {
+                return "Valid firstName";
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.INVALID_FIRSTNAME, "Invalid firstName");
+            }
         }
-        public void ValidatingPassword_4(string data)
+        public string ValidateLastName(string data)
         {
-            string result = Regex.IsMatch(data, password_4) ? $"{data}: Valid Password" : $"{data}: Invalid Password";
-
-            Console.WriteLine(result);
+            bool result = Regex.IsMatch(data, lastName);
+            if (result)
+            {
+                return "Valid lastName";
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.INVALID_LASTNAME, "Invalid lastName");
+            }
         }
-
-        public void ValidatingPassword_3(string data)
+        public string ValidateEmail(string data)
         {
-            string result = Regex.IsMatch(data, password_3) ? $"{data}: Valid Password" : $"{data}: Invalid Password";
-
-            Console.WriteLine(result);
+            bool result = Regex.IsMatch(data, email);
+            if (result)
+            {
+                return "Valid email";
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.INVALID_EMAIL, "Invalid email");
+            }
         }
-
-        public void ValidatingPassword_2(string data)
+        public string ValidateMobileNo(string data)
         {
-            string result = Regex.IsMatch(data, password_2) ? $"{data}: Valid Password" : $"{data}: InValid Password";
-
-            Console.WriteLine(result);
+            bool result = Regex.IsMatch(data, mobileNo);
+            if (result)
+            {
+                return "Valid mobileNo";
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.INVALID_MOBILE_NO, "Invalid mobileNo");
+            }
         }
-
-        public void ValidatingPassword(string data)
+        public string ValidatePassword(string data)
         {
-            string result = Regex.IsMatch(data, password) ? $"{data}: Valid Password" : $"{data}: Invalid Password";
-
-            Console.WriteLine(result);
-        }
-
-        public bool ValidatingNumber(string data)
-        {
-            bool result = Regex.IsMatch(data, mobileNo) ? true : false;
-            return result;
-
-        }
-        public bool Validating_Email(string data)
-        {
-            bool result = Regex.IsMatch(data, emailId) ? true : false;
-
-            return result;
-        }
-
-
-        public bool ValidateFirstName(string data)
-        {
-
-            bool result = Regex.IsMatch(data, firstName) ? true : false;
-
-            return result;
-
-
-        }
-
-        public bool ValidateLastName(string data)
-        {
-
-            bool result = Regex.IsMatch(data, lastName) ? true : false;
-
-            return result;
-
+            bool result = Regex.IsMatch(data, password);
+            if (result)
+            {
+                return "Valid password";
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.INVALID_PASSWORD, "Invalid password");
+            }
         }
     }
 }
